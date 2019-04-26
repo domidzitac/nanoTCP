d6043ca061c06990293be4943607cfd800b24a55
finnthedawg <42992404+finnthedawg@users.noreply.github.com>
2019-04-26 16:52:26 -0700

    Formatting and edits in rdt_reciever

--- rdt2.0/src/rdt_receiver.c
+++ rdt2.0/src/rdt_receiver.c
@@ -76,7 +76,7 @@
       * main loop: wait for a datagram, then echo it
       */
      VLOG(DEBUG, "epoch time, bytes received, sequence number");
-     /* Window is a circular queue*/
+     /* Window is a circular queue that remembers buffered recieved packets (out of order)*/
      int* window = (int*)malloc(sizeof(int)*window_size);
      int i;
      for (i = 0; i < window_size; i ++) window[i] = 0; //Initialize
@@ -98,26 +98,28 @@
          gettimeofday(&tp, NULL);
          VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
 
-         //
-         int now = fseek(fp, recvpkt->hdr.seqno - recvpkt->hdr.data_size, SEEK_SET);
+         //Write to the position of the packet we recieved
+         fseek(fp, recvpkt->hdr.seqno - recvpkt->hdr.data_size, SEEK_SET);
          fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp); //Writes packet data into fp.
          sndpkt = make_packet(0);
-         int ackno = (recvpkt->hdr.seqno- recvpkt->hdr.data_size) / DATA_SIZE;
+         int ackno = (recvpkt->hdr.seqno - recvpkt->hdr.data_size) / DATA_SIZE; //Get the number of the segment
          if (ackno >= last_ack){
+           /* We recieved some packets out of order. We can remember we saved it.
+            Queuesize circular is used because we don't recieve packets out of our window*/
              int index = (window_start + (ackno - last_ack))% window_size;
              window[index] = 1;
              int inc = 0;
              printf("last_ack=%d ackno=%d index=%d window_start=%d\n", last_ack, ackno, index, window_start);
-             while (window[window_start] == 1){
+             while (window[window_start] == 1){ //Check if we have buffered packets
                  window[window_start] = 0;
                  window_start = (window_start + 1) % window_size;
-                 inc ++;
+                 inc ++; //This is the number of "buffered packet"
              }
              printf("After window_start=%d inc=%d\n", window_start, inc);
-             last_ack = last_ack + inc;
+             last_ack = last_ack + inc; //Send back the ack of cumulative packets we have already
          }
          sndpkt->hdr.ackno = last_ack;
-         sndpkt->hdr.ctr_flags = ACK;
+         sndpkt->hdr.ctr_flags = ACK; //This is a ack response
          if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
                  (struct sockaddr *) &clientaddr, clientlen) < 0) {
              error("ERROR in sendto");
