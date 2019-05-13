0540faf574b1001b544a10916f00559c7be0c718
None <None>
2019-05-13 12:34:14 -0700

    Final plotting working

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ time=150
 dir=output
 name=projectTest
 trace=DL_2_16Mbps
+loss=0.01
 
 # project2 TCP
-python run.py -tr $trace -t $time --name $name --dir $dir
+python run.py -tr $trace -t $time --name $name --dir $dir --loss $loss
 python plot.py -tr $trace --name $name --dir $dir
